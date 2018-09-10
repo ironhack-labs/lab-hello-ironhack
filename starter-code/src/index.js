84bb117af19cc482dde7bfca3303e1c872c8acd7
trong nhan nguyen <ntnhan30@gmail.com>
2018-09-10 18:13:58 +0200

    better

--- starter-code/src/index.js
+++ starter-code/src/index.js
@@ -1,66 +1,66 @@
 // jshint ignore: start
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-      <h1>App</h1>
-      <Header></Header>
-      <Content></Content>
+        <Header />
+        <Content />
       </div>
     );
   }
 }
 
-
-
 class Header extends React.Component {
-    render() {
-      return (
-        <div className=" dark-blue">
-        <h1> Say hello to <p>
-            ReactJS</p> 
-            </h1>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div className="dark-blue">
+          <img src="images/ironhack-logo.svg" alt=""/>
+        <h1> Say hello to </h1>
+        <h1>ReactJS</h1>
+       <button>Awesome!</button>
+      </div>
+    );
   }
+}
 
-  class Content extends React.Component {
-    render() {
-      return (
-        <div className=" cards content">
-       <Card image="/images/icon1.png" title="Declarative">React makes it painless to create interactive UIs.</Card>
-       <Card image="/images/icon2.png" title="Components">Build encapsulated components that manage their state.</Card>
-       <Card image="/images/icon3.png" title="Single-Way">A set of immutable values are passed to the components.</Card>
-       <Card image="/images/icon4.png" title="JSX">Statically typed, designed to run on modern browsers.</Card>
-        </div>
-      );
-    }
+class Content extends React.Component {
+  render() {
+    return (
+      <div className=" cards content">
+        <Card image="/images/icon1.png" title="Declarative">
+          React makes it painless to create interactive UIs.
+        </Card>
+        <Card image="/images/icon2.png" title="Components">
+          Build encapsulated components that manage their state.
+        </Card>
+        <Card image="/images/icon3.png" title="Single-Way">
+          A set of immutable values are passed to the components.
+        </Card>
+        <Card image="/images/icon4.png" title="JSX">
+          Statically typed, designed to run on modern browsers.
+        </Card>
+      </div>
+    );
   }
+}
 
-  class Card extends React.Component {
-    render() {
-      return (
-        <div className="card">
-            <div className="img">
-         <img src={this.props.image} alt=""/>
-            </div>
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="card">
+        <div className="img">
+          <img src={this.props.image} alt="" />
+        </div>
         <h4>{this.props.title}</h4>
         <p>{this.props.children}</p>
-        </div>
-      );
-    }
+      </div>
+    );
   }
+}
 
-
-
-
-  ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-  );
+ReactDOM.render(<App />, document.getElementById("root"));
