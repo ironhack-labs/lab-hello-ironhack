7f08766c7cdf08dc117d7c5cf2124b88b9bad4b3
Doug DeJong <dougdejong@gmail.com>
2018-09-24 12:21:40 -0400

    added salutations

---
+++ starter-code/src/Goodbye.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./App.css";
+
+
+class Bye extends Component {
+    render() {
+        return (
+            <h1>Goodbye, World!</h1>
+        );
+  }
+}
+export default Bye;
--- starter-code/src/index.js
+++ starter-code/src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
+import Bye from "./Goodbye.js";
+
 ReactDOM.render(
   <App />,
   document.getElementById("root")
+);
+
+ReactDOM.render(
+  <Bye />,
+  document.getElementById("time")
 );
