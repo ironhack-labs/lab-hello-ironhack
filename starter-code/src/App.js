7e646e58c8e92e52cd49d280ebcc4e306a135924
Nicholas Crispino <crispinonicky@gmail.com>
2018-09-24 11:08:28 -0400

    finished bottom, finishing up top

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -8,6 +8,16 @@ class App extends Component {
 
 
       <div className="App">
+
+      <div className="navbar">
+
+<div className = "logo">
+      <img src="/images/ironhack-logo.svg"/>
+      </div>
+
+
+      </div>
+
       <div className="top">
         <h1> Say hello to ReactJS</h1>
         <p>You will learn a Frontend framework from scratch,
@@ -15,6 +25,38 @@ class App extends Component {
         </p>
         <button>Awesome!</button>
         </div>
+
+
+<div className="all-cards">
+
+<div className="card">
+<img src="./images/icon1.png"/>
+<h2>Declarative</h2>
+<a>React makes it painless to create interactive UIs.</a>
+</div>
+<div className="card">
+<img src="./images/icon2.png"/>
+<h2>Components</h2>
+<a>Build encapsulated components that manage their state.</a>
+</div>
+<div className="card">
+<img src="./images/icon3.png"/>
+<h2>Single-Way</h2>
+<a>A set of immutable values are passed to the components</a>
+</div>
+<div className="card">
+<img src="./images/icon4.png"/>
+<h2>JSX</h2>
+<a>Statically-typed. Designed to run on modern browsers.</a>
+</div>
+
+
+</div>
+
+
+
+
+
       </div>
 
 
