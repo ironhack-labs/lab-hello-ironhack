6352f2f2a3eb71a1af6423dfaef7002db90e86f7
Nicholas Crispino <crispinonicky@gmail.com>
2018-09-24 10:45:09 -0400

    top part nearly done

---
+++ starter-code/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+
+
+
+      <div className="App">
+      <div className="top">
+        <h1> Say hello to ReactJS</h1>
+        <p>You will learn a Frontend framework from scratch,
+          to become a Ninka Developer.
+        </p>
+        <button>Awesome!</button>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+    );
+  }
+}
+
+export default App;
