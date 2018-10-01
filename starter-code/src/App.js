0b0277e5860c3d1f8eb46debfe9363176c76bd82
mariaalons <calpurniavee@gmail.com>
2018-10-01 16:46:55 +0200

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,32 +1,47 @@
 import React, { Component } from "react";
 
 
-
-
 export const App = () => {
   return (
+    <div>
   <div style={{backgroundColor:"#242442"}}>
-    <ImgComponent2/>
-    <ImgComponent/>
+   <ImgComponent src='images/ironhack-logo.svg'/>
+    <ImgComponent src='images/menu-top.svg'/>
   <h1 style={{color:"white"}}>Say Hello to ReactJS</h1>
   <h3 style={{color:"white"}}>You will learn a Frontend<br></br>
   framework from scrach, to<br></br> 
   become a Ninja Developer</h3>
-
-  </div>)
+  <button style={{backgroundColor:"white", color:"#242442"}} >Awesome!</button>
+  </div>
+  <BlockComponent src='images/icon1.png' title='Declarative' text='Lorem fistrum no puedor va usté muy cargadoo pecador me cago en tus muelas de la pradera.'/>
+  <BlockComponent src='images/icon2.png' title='Components' text='Lorem fistrum no puedor va usté muy cargadoo pecador me cago en tus muelas de la pradera.'/>
+  <BlockComponent src='images/icon3.png' title='Single-Way' text='Lorem fistrum no puedor va usté muy cargadoo pecador me cago en tus muelas de la pradera.'/>
+  <BlockComponent src='images/icon4.png' title='JSX' text='Lorem fistrum no puedor va usté muy cargadoo pecador me cago en tus muelas de la pradera.'/>
+  </div>
+  )
 };
 
-const ImgComponent = () => {
+const ImgComponent = (props, props2) => {
  return (
-   <img src='images/menu-top.svg'></img>
+   <img src={props.src}></img>
  )
  };
 
- const ImgComponent2 = () => {
-  return (
-    <img src='images/ironhack-logo.svg'></img>
-  )
-  };
+ const BlockComponent = (props) => {
+   return (
+    <div>
+    <img src={props.src}></img>
+    <h3>{props.title}</h3>
+    <p>{props.text}</p>
+   </div>
+   )
+ }
+
+
+
+
+
+
 
 
 
