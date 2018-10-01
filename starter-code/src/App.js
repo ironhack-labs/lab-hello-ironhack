58921b6b426c4a78702b5e4fc84868738aa5a778
Carnelas <carnelas@gmail.com>
2018-10-01 16:06:44 +0200

    finish

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -7,6 +7,7 @@ export class App extends Component {
       <div id="body">
         <div id="top">
           {/* div superior */}
+          <div id="header">
           <div id="logo">
             <img src="./images/ironhack-logo.svg"></img>
           </div>
@@ -14,19 +15,36 @@ export class App extends Component {
             <img src="./images/menu-top.svg"></img>
           </div>
         </div>
+        </div>
         <div id="middle">
         <div id="helloworld">
-        Say Hello to ReactJS
+        <h1>Say Hello to ReactJS</h1>
         </div>
-        <h4>You will learn a Fronted framework from scratch, to becaome an Ninka Developer</h4>
+        <p>You will learn a Fronted framework from scratch, to becaome an Ninka Developer</p>
         <button>Awesome!</button>
         </div>
+
         <div id="bottom">
           {/* div inferior */}
           <div classname="icon">
           <img src="./images/icon1.png"></img>
           <h2>Declarative</h2>
             <p>React makes it painless to create interactive UIs.</p>
+        </div>
+        <div classname="icon">
+          <img src="./images/icon2.png"></img>
+          <h2>Components</h2>
+            <p>Build encapsulated components that manage their state.</p>
+        </div>
+        <div classname="icon">
+          <img src="./images/icon3.png"></img>
+          <h2>Sigle-way</h2>
+            <p>A set of inmutable values are passed to the component's.</p>
+        </div>
+        <div classname="icon">
+          <img src="./images/icon4.png"></img>
+          <h2>JSX</h2>
+            <p>Statically-typed, designed to run on modern browsers.</p>
         </div>
       </div>
       </div>
