abd94609db81e6ea10aeb45ae64eaf3dbff81f9c
paulagonzalez <paula.gd@hotmail.es>
2018-10-02 09:11:47 +0200

    Done

--- starter-code/src/section.js
+++ starter-code/src/section.js
@@ -1,15 +1,5 @@
 import React from 'react';
 
-/* export const Section = () => {
-    return (
-        <div width="25%">
-            <img src="../images/icon1.png" />
-            <h2> Declarative </h2>
-            <p> React makes it <br />painless to create <br />interactive UIs. </p>
-        </div>
-    )
-} */
-
 export const Section = ({ image, title, p1, p2, p3 }) => {
     return (
         <div width="25%" style={sectionStyles}>
