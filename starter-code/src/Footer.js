7e8e031b836b7db2159eced69bec29ad6c7390e4
Adolfo Vela <vela@tekplanner.com>
2018-11-14 08:54:49 -0600

    Arreglo del div del footer

--- starter-code/src/Footer.js
+++ starter-code/src/Footer.js
@@ -5,28 +5,28 @@ class Footer extends Component{
     render(){
         return(
             <div className='container'>
-                <div>
+                <div className='divFooter'>
                     <img src="images/icon1.png" alt=""/>
                     <h2>Declarative</h2>
                     <p>React makes it
                     paintless to create
                     interactive UI</p>
                 </div>
-                <div>
+                <div className='divFooter'>
                     <img src="images/icon2.png" alt=""/>
                     <h2>Components</h2>
                     <p>Buil encapsulated
                     components that
                     manage their state</p>
                 </div>
-                <div>
+                <div className='divFooter'>
                     <img src="images/icon3.png" alt=""/>
                     <h2>Single-Way</h2>
                     <p>A set of immutable
                         values are passed to
                         the component's</p>
                 </div>
-                <div>
+                <div className='divFooter'>
                     <img src="images/icon4.png" alt=""/>
                     <h2>JSX</h2>
                     <p>Statically-typed
