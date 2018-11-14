2b066d6312b3ac18b563f2765aa31571f8caeaf1
Ricardo Mayo <rmayo.1995@gmail.com>
2018-11-14 17:25:37 -0600

    done

--- starter-code/src/Abajo.js
+++ starter-code/src/Abajo.js
@@ -5,28 +5,28 @@ import './Abajo.css';
 class Abajo extends Component {
 	render() {
 		return (
-			<div>
-				<div>
+			<div className="abajo">
+				<div className="card">
 					{/* <img src={icon1} alt=""/> */}
 					<img src={'images/icon1.png'} />
 					<h3>Declarative</h3>
 					<p>React makes it painless to create interactive UIs.</p>
 				</div>
-				<div>
+				<div className="card">
 					<img src={'images/icon2.png'} />
 					<h3>Components</h3>
 					<p>
 						Build encapsulated components that manage their state.
 					</p>
 				</div>
-				<div>
+				<div className="card">
 					<img src={'images/icon3.png'} />
 					<h3>Single-Way</h3>
 					<p>
 						A set of immutable values are passed to the component's.
 					</p>
 				</div>
-				<div>
+				<div className="card">
 					<img src={'images/icon4.png'} />
 					<h3>JSX</h3>
 					<p>Statically-typed, designed to run on modern browsers.</p>
--- starter-code/src/Arriba.js
+++ starter-code/src/Arriba.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
 
-
 import './Arriba.css';
 
 class Arriba extends Component {
+	render() {
+		return (
+			<div className="arriba">
+				<ul>
+					<li>
+						<img src="images/ironhack-logo.svg" />
+					</li>
+					<li className="menu">
+						<img src="images/menu-top.svg" />
+					</li>
+				</ul>
+				<ul>
+					<li>
+						<div className="text">
+							<h1 className="title">Say hello to ReactJS</h1>
 
-  render() {
-    return (
-      <div className="arriba">
-        <img src='images/ironhack-logo.svg' />
-                <img src='images/menu-top.svg' />
-                <img src='images/react-logo.svg' />
-        <h1>Say hello to</h1>
-        <h1>ReactJS</h1>
-        <p>You will learn a Frontend</p>
-        <p>framework from scratch, to</p>
-        <p>becaome an Ninka Developer.</p>
-        <button>Awesome!</button>
-      </div>
-    )
-  }
+							<p>
+								You will learn a Frontend framework from
+								scratch, to becaome an Ninka Developer.
+							</p>
 
+							<button>Awesome!</button>
+						</div>
+					</li>
+                        <img src="images/react-logo.svg" />
+					<li />
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default Arriba;
