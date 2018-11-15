902298de69bcd5c676c5effa3aa4a3b9708f0653
Ximena Perez <ximxim25@gmail.com>
2018-11-14 22:26:58 -0600

    component listo

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
 import Cards from './Cards';
+import Landing from './Landing';
 
 class App extends Component {
   render() {
     return (
+
       <div>
+        <Landing />
         <Cards />
       </div>
-    );
+    )
   }
 }
 
