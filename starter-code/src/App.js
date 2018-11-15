d0bb4885946e3e3626555507f7150f26e11c82c7
Pedro Cruz <pedro.cruz@blackinntech.com>
2018-11-14 23:42:26 -0600

    Finished styles

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
-import './App.css';
 import First from './First';
 import Second from './Second';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
+            <div>
                 <First/>
                 <Second/>
             </div>
--- starter-code/src/First.js
+++ starter-code/src/First.js
@@ -5,15 +5,19 @@ class First extends Component{
     render(){
         return (
             <header className='header'>
-                <nav className='nav'>
+                <nav className='navbar'>
                     <img src="images/ironhack-logo.svg"></img>
                     <img src="images/menu-top.svg"></img>
                 </nav>
                 <div className='content'>
-                    <h1>Say hello to ReactJS</h1>
-                    <img src="images/react-logo.svg"></img>
-                    <p>You will learn a Frontend framework from scratch, to became a Ninja Developer</p>
-                    <button>Awesome!</button>
+                    <div>
+                        <h1>Say hello to ReactJS</h1>
+                        <p>You will learn a Frontend framework from scratch, to became a Ninja Developer</p>
+                        <button className='button'>Awesome!</button>
+                    </div>
+                    <div>
+                        <img src="images/react-logo.svg"></img>
+                    </div>
                 </div>
             </header>
         )
