9ef7402bb94c5d9282d9c2792376c0272076800e
joaoeduardo81 <macbookair@MacBooks-MacBook-Air.local>
2018-11-26 16:54:46 -0200

    done

--- starter-code/public/Landing.js
+++ starter-code/public/Landing.js
@@ -2,10 +2,11 @@ import React from "react";
 import Navigation from "./Navigation.js";
 import Title from "./Title.js";
 import Box from "./Box.js";
+import "./Landing.css";
 
 const Landing = () => {
     return (
-      <div>
+      <div className="Landing">
         <Navigation />
         <Title />
         <Box />
