31376004043391889f710fa9dce88f53fa754798
mannysin <sinclair2610@gmail.com>
2018-12-03 13:55:04 -0500

    Added 2nd component

--- starter-code/hello-ironhack/src/App.js
+++ starter-code/hello-ironhack/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Icons from './Icons';
 
 class App extends Component {
   render() {
@@ -32,38 +33,10 @@ class App extends Component {
     </header>
     )
     
-    const bottomDiv = 
-    (
-    <div className="bottomTextTitles">
-    <div>
-      <img src='images/icon1.png' alt="logo" />
-      <h2> Declarative</h2>
-      <h4> React makes it <br/> painless to create <br/> interactive UIs.</h4>
-    </div> 
-    <div>
-      <img src='images/icon2.png' alt="logo" />
-      <h2> Components</h2>
-      <h4> Build encapsulated <br/> components that <br/> manage their state.</h4>
-    </div>
-    <div> 
-      <img src='images/icon3.png' alt="logo" />
-      <h2> Single-Way</h2>
-      <h4> A set of immutable <br/> values are passed to <br/> the components.</h4>
-    </div>
-    <div>
-
-      <img src='images/icon4.png' alt="logo" />
-      <h2> JSX</h2>
-      <h4> Statically-typed, <br/> designed to run on <br/> modern browsers.</h4>
-    </div>
-    
-    </div>
-    )
-
     return (
       <div className="App">
         {topDiv}
-        {bottomDiv}
+        <Icons />
       </div>
     );
   }
--- starter-code/hello-ironhack/src/Icons.js
+++ starter-code/hello-ironhack/src/Icons.js
@@ -4,11 +4,36 @@ import './App.css';
 
 class Icons extends Component {
     render() {
+      const bottomDiv = 
+      (
+      <div className="bottomTextTitles">
+      <div>
+        <img src='images/icon1.png' alt="logo" />
+        <h2> Declarative</h2>
+        <h4> React makes it <br/> painless to create <br/> interactive UIs.</h4>
+      </div> 
+      <div>
+        <img src='images/icon2.png' alt="logo" />
+        <h2> Components</h2>
+        <h4> Build encapsulated <br/> components that <br/> manage their state.</h4>
+      </div>
+      <div> 
+        <img src='images/icon3.png' alt="logo" />
+        <h2> Single-Way</h2>
+        <h4> A set of immutable <br/> values are passed to <br/> the components.</h4>
+      </div>
+      <div>
+  
+        <img src='images/icon4.png' alt="logo" />
+        <h2> JSX</h2>
+        <h4> Statically-typed, <br/> designed to run on <br/> modern browsers.</h4>
+      </div>
       
+      </div>
+      )
   
       return (
         <div className="App">
-          {topDiv}
           {bottomDiv}
         </div>
       );
