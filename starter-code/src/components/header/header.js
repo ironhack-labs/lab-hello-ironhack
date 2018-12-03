a034c86c6c320c8fdf93134969a672daab3ad041
RuddyLotar <ruddylotar@gmail.com>
2018-12-03 15:54:31 +0100

    first component

---
+++ starter-code/src/components/header/header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./style.css";
+
+class Header extends React.Component {
+
+
+  render() {
+
+    
+    return (
+      <div id="header" className="Header">
+      
+        <img class="iron-logo" src="images/ironhack-logo.svg" alt="Ironhack-logo"/>
+        <img class="menu-top" src="images/menu-top.svg" alt="Menu-logo"/><br/><br/>
+        <h1>Say hello to ReactJS</h1>
+        <p>You will learn a Frontend framework from scratch, to become an Ninka Developer</p>
+        <button>Awesome!</button>
+        
+        
+      </div>
+    );
+  }
+}
+
+export default Header;
