e9248792f2e249d66a6cc9ac2289147adc8be958
Omar Avelar <omar.avelar.f@gmail.com>
2018-12-10 16:59:31 -0600

    done red bk

---
+++ starter-code/src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import '../public/styless/styless.css'
+
+class App extends Component{
+
+render() {
+  return (
+    <div className="perro" >
+    <img src="../images/icon1.png" alt="" />
+      Hello holi!
+
+    </div>
+  )
+}
+
+}
+
+export default App;
