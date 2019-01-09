11f72672e5d5fa1a4dfeced5f42423c933d6c535
Teresa Romero <tromerolozano@gmail.com>
2019-01-09 10:43:03 +0100

    css done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -5,12 +5,44 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="header">
+        <img src="./images/ironhack-logo.svg"></img>
+        <img src="./images/menu-top.svg"></img>
+        </div>
+        <div className="sectionOne">
+          <h1>Say hello to ReactJS</h1>
+          <p>You will learn a Frontend framework from scratch, to become an Ninka Developer.</p>
+          <a href="#">Awesome!</a>
+        </div>
+        <div className="sectionTwo">
+        <div class="row">
+        <div class="col-4">
+          <img src="./images/icon1.png" alt=""/>
+          <h2>Declarative</h2>
+          <p>React makes it painless to create interactive UIs</p>
+        </div>
+        <div class="col-4">
+          <img src="./images/icon2.png" alt=""/>
+          <h2>Components</h2>
+          <p>Build encapsulated components that manage their state.</p>
+        </div>
+        <div class="col-4">
+          <img src="./images/icon3.png" alt=""/>
+          <h2>Single-Way</h2>
+          <p>A set of immutable values are passed to the component's</p>
+        </div>
+        <div class="col-4">
+          <img src="./images/icon4.png" alt=""/>
+          <h2>JSX</h2>
+          <p>Statucally-typed designed to run on modern browsers</p>
+        </div>
 
-        <img src="./images/icon1.png"></img>
-        <h1> Hello Ironhackers! </h1>
+        </div>
+        </div>
       </div>
     );
   }
 }
+
 
 export default App;
