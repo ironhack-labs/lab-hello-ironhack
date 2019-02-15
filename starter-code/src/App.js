3cc0634d70376957900d6c254a08087227ee3957
Susanne Tünker <susanne.tuenker@hotmail.de>
2019-02-15 14:38:27 +0100

    done

---
+++ starter-code/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css"
+
+function Hello() {
+  
+
+  return  <div className="outerCon">
+  <h1>Say hello to ReactJS</h1>
+  <h3>You will learn a Frontend <br />
+  framework from scratch, to <br />
+  become a Ninja Developer</h3>
+
+  <button>Awesome</button>
+  </div>
+
+
+}
+
+export default Hello;
