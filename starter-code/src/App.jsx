991c9a77e9f7a09356cdac6de053f02b46bc9c73
diegormedia <diegoramos.media@gmail.com>
2019-02-17 01:02:30 -0300

    Lab Hello Ironhack - done

---
+++ starter-code/src/App.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import AppStyle from "./AppStyle.css"
+
+function App() {
+  return (
+    <div className="App">
+      <section className="container-blue">
+        <div class="row text-center">
+          <div class="col-12 col-md-12">
+            <h1 class="display-3">Hello ReactJS</h1>
+            <p>You will learn a Frontend framework from scratch, to becaome an Ninja Developer</p>
+            <div className="button-link">
+              <a href="/">Awesome!</a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="container container-white">
+        <div class="row text-center">
+          <div class="col-12 col-md-3">
+            <img src="/images/icon1.png" alt="" />
+            <h2>Declarative</h2>
+            <p>React makes it painless to create interactive UIs</p>
+          </div>
+          <div class="col-12 col-md-3">
+            <img src="/images/icon2.png" alt="" />
+            <h2>Components</h2>
+            <p>Build encapsulated components that manage their state</p>
+          </div>
+          <div class="col-12 col-md-3">
+            <img src="/images/icon3.png" alt="" />
+            <h2>Single-Way</h2>
+            <p>A set of immutable values are passed to the component's</p>
+          </div>
+          <div class="col-12 col-md-3">
+            <img src="/images/icon4.png" alt="" />
+            <h2>JSX</h2>
+            <p>Statically-typed designed to run on modern browsers</p>
+          </div>
+        </div>
+        <small>
+          Developer:
+          <a href="http://github.com/diegormedia">Diego Ramos</a>
+        </small>
+      </section>
+    </div>
+  );
+}
+
+export default App;
