bfae7cf68e47271cd8bf7f6a80a011f3d740661b
Víctor Bañuls <vicbansax@gmail.com>
2019-02-19 09:34:41 +0100

    Fixed one thing

--- starter-code/src/Object.js
+++ starter-code/src/Object.js
@@ -2,7 +2,7 @@ import React from "react";
 
 
 function Objects (props){
-  return <div><img src={props.img}/><h3 className="title-obj">{props.title}</h3><p className="desc-obj">{props.desc}</p></div>
+  return <div><img src={props.img}/><h3>{props.title}</h3><p>{props.desc}</p></div>
 }
 
 export default Objects;
