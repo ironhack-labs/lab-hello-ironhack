e329f3f9a4ba4f9d3e619c1056cfbd7448db7cea
Rita Mernissi <mernissi.rita@gmail.com>
2019-02-20 12:29:07 +0100

    added svg files

--- starter-code/src/Nav.js
+++ starter-code/src/Nav.js
@@ -4,8 +4,8 @@ export class Nav extends React.Component {
   render() {
     return (
       <ul>
-        <li> <img id="logo-ih" src="https://course_report_production.s3.amazonaws.com/rich/rich_files/rich_files/827/s200/ironhacklogo-compressor-20-1-.png"/> </li>
-        <li id="burger-menu"> <img src="/images/burger-menu.png" id="burger-menu"/> </li>
+        <li> <img id="logo-ih" src="/images/ironhack-logo.svg"/> </li>
+        <li id="burger-menu"> <img src="/images/menu-top.svg" id="burger-menu"/> </li>
       </ul>
     );
   }
