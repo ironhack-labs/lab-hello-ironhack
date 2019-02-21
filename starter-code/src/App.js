f32165c20f3d94a31040aac4344c28c58ce04ad0
Paola Mejia <paolamejia@ANNAMEJIA.local>
2019-02-21 17:08:11 -0600

    nuevos ajustes UI

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -9,12 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <Nav />
-          <section class="up">
-            <h1> Say hello ReactJS </h1>
-            <h2> You will learn a front end framework from scratch, to become a Ninja Developer</h2>
-            <button type="submit" name="button" class="btn btn-red">Awsome!</button>
-          </section>
+          < Nav />
           < Cards />
         </div>
 
--- starter-code/src/Nav.js
+++ starter-code/src/Nav.js
@@ -1,12 +1,34 @@
 import React, { Component } from "react";
 
-class Nav extends Component{
-  render(){
-    return(
-      <nav>
-        <img src="./images/ironhack-logo.svg"></img>
-      </nav>
+class Nav extends Component {
+  render() {
+    return (
+      <div className="up">
+        <nav>
+          <img src="../images/ironhack-logo.svg" alt="" />
+          <img src="../images/menu-top.svg" alt="" />
+        </nav>
 
+        <section className="section-one">
+          <div className="text">
+            <h1>Say hello to ReactJS</h1>
+            <h2>
+              You will learn a Frontend framework from scratch, to became an
+              Ninja Developer
+            </h2>
+            <button class="btn">Awesome!</button>
+          </div>
+
+          <div className="background">
+            <div className="left-icons-images">
+              <img src="../images/react-logo.svg" alt="" />
+              <img src="../images/react-logo.svg" alt="" />
+              <img src="../images/react-logo.svg" alt="" />
+            </div>
+
+          </div>
+        </section>
+      </div>
     );
   }
 }
