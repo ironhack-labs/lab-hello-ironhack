39eb37b7eb82126579537a16fdc4e64aa96c3676
cameron-chromiak <cam.chromiak@hotmail.com>
2019-03-04 11:19:50 -0500

    Pages are connected

---
+++ starter-code/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Page from './components/page.jsx'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> Hello Ironhackers! </h1>
+        <Page/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ starter-code/src/components/page.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Page extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Container</h1>
+      </div>
+    );
+  }
+}
+
+export default Page;
