8e189208f212d6f8c211f0f8987d42aa9ecb34cb
Gustavo Galvez <gustavo.galvez09@gmail.com>
2019-03-20 00:53:41 -0400

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -6,12 +6,36 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div class="first-block">
+        <div className="first-block">
           <img src="../images/ironhack-logo.svg"></img>
-          <h1> Say hello to ReactsJS </h1>
-          <h2> <span>You will learn a Frontend framework from scratch, to become a Ninja Developper</h2>
+          <h1> Say hello to</h1>
+          <h1> ReactsJS </h1>
+          <span> You will learn a Frontend</span><br></br>
+          <span> framework from scratch, to</span><br></br>
+          <span> become a Ninja Developer</span><br></br>
           <button className="button">Awesome!</button>
         </div>
+        <div className="second-block">
+            <img src="../images/icon1.png"></img>
+            <img src="../images/icon2.png"></img>
+            <img src="../images/icon3.png"></img>
+            <img src="../images/icon4.png"></img>
+        </div>
+
+        <div className="third-block">
+            <span>Declarative</span>
+            <span>Components</span>
+            <span>Single-Way</span>
+            <span>JSX</span>
+        </div>
+
+        <div className="fourth-block">
+            <span>React makes it painless to create interactive UIs</span>
+            <span>Build encapsulated components that manage their state</span>
+            <span> A set of immutable values are passed to the components</span>
+            <span>Statically typed designed to run on modern browsers</span>
+        </div>
+
       </div>
     );
   }
