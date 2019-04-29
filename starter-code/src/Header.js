5ed125c982ad247617752cf83bd37e14258fd437
Jesus Jimenez <jmnz.jesus@gmail.com>
2019-04-29 15:08:09 +0200

    JJU Berlin

---
+++ starter-code/src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './header.css';
+
+const Header = props => {
+    return (
+        <div className="header">
+            <div className="header-container">
+                <div className="header-title">
+                    <h1>Say Hello to ReactJS</h1>
+                </div>
+                <div className="header-subtitle">
+                    <h3>
+                        You will learn a Frontend framework from scratch, to become a Ninja Developer
+                    </h3>
+                </div>
+                <div className="header-btn">
+                    <a href="./">Awesome!</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
