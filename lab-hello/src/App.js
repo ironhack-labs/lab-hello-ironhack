bcabadd977a0fec843371d036b22754ab43c7dae
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2019-04-29 15:44:15 +0200

    set environement and main text done

---
+++ lab-hello/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Main from './main/main'
+
+import './App.css'
+
+function App () {
+  return (
+    <div className='App'>
+      <header className='App-header' />
+      <Main />
+    </div>
+  )
+}
+
+export default App
---
+++ lab-hello/src/main/main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './main.css'
+
+export default function foo (props) {
+  return (
+    <div className='main'>
+      <h1>Say Hello to REACTJS </h1>
+      <h4>
+        You will learn a Frontend framework from scratch, to became an Ninka
+        Developer.
+      </h4>
+    </div>
+  )
+}
