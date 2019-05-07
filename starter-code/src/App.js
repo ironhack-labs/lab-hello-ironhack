5e6943bdf7907b9cb9b6b5d07d3b7e45d6d8fe49
Jefferson IS Lima <jeffersoninacio@hotmail.com>
2019-05-06 21:54:17 -0300

    Done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -58,19 +58,22 @@ class App extends Component {
           <div className="icons">
             {icon1}
             <h1>Declarative</h1>
-            <p>hjvasjvjvjvjvjvgjvguj</p>
-
+            <p>React makes it painless to create interactive UIs.</p>
+          </div>
+          <div className="icons">
             {icon2}
-            <h1>Declarative</h1>
-            <p>hjvasjvjvjvjvjvgjvguj</p>
-
+            <h1>Components</h1>
+            <p>Build encapsulated components that manage their state.</p>
+          </div>
+          <div className="icons">
             {icon3}
-            <h1>Declarative</h1>
-            <p>hjvasjvjvjvjvjvgjvguj</p>
-
+            <h1>Single-Way</h1>
+            <p>A set of immutable values are passed to the component´s.</p>
+          </div>
+          <div className="icons">
             {icon4}
-            <h1>Declarative</h1>
-            <p>hjvasjvjvjvjvjvgjvguj</p>
+            <h1>JSX</h1>
+            <p>Statically-typed, designed to run on modern browsers.</p>
           </div>
         </section>
       </div>
