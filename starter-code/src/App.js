577a4f7d515d4781359a61e5e62077c03d98127c
Renan Soares <renan.soares.francisco@gmail.com>
2019-07-22 14:50:10 -0300

    rfranciscos

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -4,18 +4,42 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="Nav">
-          <img className="logo" src="./images/ironhack-logo.svg" />
-          <img className="logo2" src="/images/menu-top.svg" />
+      <div>
+        <div className="App">
+          <div className="Nav">
+            <img className="logo" src="./images/ironhack-logo.svg" />
+            <img className="logo2" src="/images/menu-top.svg" />
+          </div>
+          <div className="container">
+            <h1 className="h1"> Say hello to RectJS </h1>
+            <p className="p">
+              You will learn a Frontend framework from scratch, to becaome an
+              Ninka Developer.
+            </p>
+            <button className="btn">Awesome!</button>
+          </div>
         </div>
-        <div className="container">
-          <h1 className="h1"> Say hello to RectJS </h1>
-          <p className="p">
-            You will learn a Frontend framework from scratch, to becaome an
-            Ninka Developer.
-          </p>
-          <button className="btn">Awesome!</button>
+        <div className="content">
+          <div>
+            <img className="logo" src="/images/icon1.png" />
+            <h4>Declaritive</h4>
+            <p>React makes ir painless to create interactive Uls.</p>
+          </div>
+          <div>
+            <img className="logo" src="/images/icon2.png" />
+            <h4>Componets</h4>
+            <p>Build encapsulated componets that manage</p>
+          </div>
+          <div>
+            <img className="logo" src="/images/icon3.png" />
+            <h4></h4>
+            <p></p>
+          </div>
+          <div>
+            <img className="logo" src="/images/icon4.png" />
+            <h4></h4>
+            <p></p>
+          </div>
         </div>
       </div>
     );
