38a0b91f632262dd74f87ba041fbd7bdf40baf46
Mônica Damasceno <monica.damasceno@gitlab.com>
2019-10-14 14:18:04 -0300

    layout - "first" page

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -4,8 +4,18 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1> Hello Ironhackers! </h1>
+      <div className="home">
+        <div className="header">
+          <img src="images/ironhack-logo.svg" />
+          <img src="images/menu-top.svg" />
+        </div>
+        <div className="title">
+            <h1>Say hello to ReactJS </h1>
+            <h3> You will learn a Frontend framework from scratch, to become an Ninja Developer</h3>
+        </div>
+        <div className="fake-btn"> 
+          <span>Awesome!</span>
+        </div>
       </div>
     );
   }
