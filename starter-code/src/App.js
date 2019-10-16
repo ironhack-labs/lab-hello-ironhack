97f669f91cf6e22b6e8e4b9d584f6de408a4a2b5
cbut <c.butenschoen@gmail.com>
2019-10-16 21:20:35 +0200

    fixes columns, colors, spacing, images

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,54 +1,77 @@
 import React, { Component } from "react";
 import "./App.css";
 
-
 class App extends Component {
-    render() {
-        const ironhackLogo = "/images/ironhack-logo.svg"
-        const icon1 = "/images/icon1.png"
-        const icon2 = "/images/icon2.png"
-        const icon3 = "/images/icon3.png"
-        const greetingText = <h1>Say Hello to ReactJS which is not reactive on windows localhost pffffff</h1>
-        const ninka = <h4>you will learn react and become a ninka</h4>
-        const stupidButton = <button type="button" id="awesome">awesome</button>
-        const declarative = <h2>Declatarative</h2>
-        const icon1Text = <h3>react makes it painless to create stuff</h3>
-        const components = <h2>components</h2>
-        const icon2Text = <h3>build encapsulated components</h3>
-        const singleWay = <h2>single-way</h2>
-        const icon3Text = <h3>a SET OF IMMUTABLE VALUES</h3>
-        return (
-            <div className="App">
+  render() {
+    const ironhackLogo = "/images/ironhack-logo.svg";
+    const menu_top = "/images/menu-top.svg";
+    const background_image = "/images/react-logo.svg";
+    const icon1 = "/images/icon1.png";
+    const icon2 = "/images/icon2.png";
+    const icon3 = "/images/icon3.png";
+    const icon4 = "/images/icon4.png";
+    const greetingText = (
+      <h1>
+        Say Hello to ReactJS which is not reactive on windows localhost pffffff
+      </h1>
+    );
+    const ninka = <h4>you will learn react and become a ninka</h4>;
+    const stupidButton = (
+      <button type="button" id="awesome">
+        Awesome!
+      </button>
+    );
+    const declarative = <h2>Declatarative</h2>;
+    const icon1Text = <p>react makes it painless to create stuff</p>;
+    const components = <h2>components</h2>;
+    const icon2Text = <p>build encapsulated components</p>;
+    const singleWay = <h2>single-way</h2>;
+    const icon3Text = <p>a SET OF IMMUTABLE VALUES</p>;
+    const JSXHeadline = <h2>JSX</h2>;
+    const icon4Text = (
+      <p>Statically-typed, designed to run on modern browsers</p>
+    );
+    return (
+      <div className="App">
+        <div className="upperBlock">
+          <div className="menu">
+            <img width="50" height="50" src={ironhackLogo}></img>
+            <img src={menu_top}></img>
+          </div>
+          <div className="textInUpperBlock">
+            {greetingText}
+            {ninka}
+          </div>
 
-                <img width="100" height="100" src={ironhackLogo}></img>
-                {greetingText}
-                {ninka}
-                {stupidButton}
-                <div className="row">
-                    <div className="column"></div>
-                    {declarative}
-                    <br></br>
-                    {icon1Text}
-                    <br></br>
-                    <img src={icon1}></img>
-                    <div className="column"></div>
-                    {components}
-                    <br></br>
-                    {icon2Text}
-                    <br></br>
-                    <img src={icon2}></img>
-                    <div className="column"></div>
-                    {singleWay}
-                    <br></br>
-                    {icon3Text}
-                    <br></br>
-                    <img src={icon3}></img>
-                    <div className="column"></div>
-                </div>
-
+          {stupidButton}
+        </div>
+        <div className="lowerBlock">
+          <div className="row">
+            <div className="column">
+              <img src={icon1}></img>
+              {declarative}
+              {icon1Text}
             </div>
-        );
-    }
+            <div className="column">
+              <img src={icon2}></img>
+              {components}
+              {icon2Text}
+            </div>
+            <div className="column">
+              <img src={icon3}></img>
+              {singleWay}
+              {icon3Text}
+            </div>
+            <div className="column">
+              <img src={icon4}></img>
+              {JSXHeadline}
+              {icon4Text}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
