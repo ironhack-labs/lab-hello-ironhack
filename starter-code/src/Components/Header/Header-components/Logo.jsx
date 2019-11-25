02995945866500babc11054818f091f91a4c2889
Camilosorio <tech@camilo.fr>
2019-11-25 22:08:38 +0100

    Adding the React structure + Create first compon

---
+++ starter-code/src/Components/Header/Header-components/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import logo from '../../../../public/images/icon1.png'
+
+export default function Logo() {
+    return (
+        <div className='brand'>YOLOdsfdsfdsfds
+            <img src={logo} alt="Iron-Hack-logo" />
+        </div>
+    )
+}
---
+++ starter-code/src/Components/Header/HeaderMain.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Logo from "./../Header/Header-components/Logo"
+
+export default function HeaderMain() {
+    return (
+        <header>
+            <Logo />
+            sdfsdfdsfz
+        </header>
+    )
+}
