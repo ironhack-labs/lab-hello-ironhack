a1b16434d84950f23359f0032be7e8b18cb68f3b
roberto araya gil <naggas@Naggas.local>
2020-01-09 11:55:00 +0100

    done

--- starter-code/src/Item.js
+++ starter-code/src/Item.js
@@ -1,10 +1,11 @@
 import React from "react";
 import List from './List'
+import './Item.css'
 
 const Item = props => {
     const {title, image, description} = props
 
-  return (<div className="Item-container">
+  return (<div className="itemContainer">
     <img src={image}></img>
     <h4>{title}</h4>
     <p>{description}</p>
