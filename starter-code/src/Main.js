156fb3af61a004847f9e45f8928dfccb5b8bc936
roberto araya gil <naggas@Naggas.local>
2020-01-09 11:41:35 +0100

    header first version done

--- starter-code/src/Main.js
+++ starter-code/src/Main.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Menu from './Menu'
+import Header from './Header'
 import './Main.css'
 
 const Main = () => {
     return ( 
         <div className="mainContainer">
             <Menu/>
+            <Header/>
         </div>
      );
 }
