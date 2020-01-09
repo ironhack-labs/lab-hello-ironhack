abdb12782273085000987ee22cf999d9299d07e1
roberto araya gil <naggas@Naggas.local>
2020-01-09 11:21:44 +0100

    menu ok

--- starter-code/src/Main.js
+++ starter-code/src/Main.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Menu from './Menu'
 import './Main.css'
 
 const Main = () => {
     return ( 
         <div className="mainContainer">
-            hellow
+            <Menu/>
         </div>
      );
 }
---
+++ starter-code/src/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Menu.css'
+
+const Menu = () => {
+    return ( 
+        <div className="menu">
+            <img src="images/ironhack-logo.svg"></img>
+            <img src="images/menu-top.svg"/>
+        </div>
+     );
+}
+ 
+export default Menu;
