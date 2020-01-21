ed905e541f07a90d877d5e772d56112eb0223d46
Alison <alisonbessa@gmail.com>
2020-01-20 23:18:04 -0300

    finishing frontend

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="container">
-          <nav className="navbar navbar-light bg-dark">
+          <nav className="navbar">
             <a className="navbar-brand"><img src='/images/ironhack-logo.svg'></img></a>
             <form className="form-inline">
               <button className="btn btn-outline-success my-2 my-sm-0" type="submit"><img src='/images/menu-top.svg'></img></button>
@@ -17,28 +17,29 @@ class App extends Component {
               <h1>Say hello to<br/>
               ReactJS
               </h1>
-              <h2>You will learn a Frontend <br>
-              </br> framework from scratch, to <br>
-              </br> becaome an Ninka Developer.
-              </h2>
-              <button className= 'btn btn-outline-light bg-light' type='submit'>Awesome!</button>
+              <h6>You will learn a Frontend <br/>
+              framework from scratch, to <br/>
+              becaome an Ninka Developer.
+              </h6> <br/><br/>
+              <button className= 'btn btn-outline-light bg-light text-dark btn-lg' type='submit'>Awesome!</button>
             </div>
           </section>
         </div>
-        <div className="Icons">
+        <div class="d-flex justify-content-around content">
           <div>
-            
+            <img src="../images/icon1.png"/>
           </div>
           <div>
-            
+            <img src="../images/icon2.png"/>
           </div>
           <div>
-            
+            <img src="../images/icon3.png"/>
           </div>
           <div>
-            
+            <img src="../images/icon4.png"/>
           </div>
         </div>
+
       </div>
     );
   }
