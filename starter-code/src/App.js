9412f5483dd56f628b6f6d32c12a2c90470ed098
Chanty He <chanty.he90@googlemail.com>
2020-02-06 16:56:07 +0100

    styling

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -11,21 +11,21 @@ class App extends Component {
     // ]
     let liste = [
       {
-        title: "Dec",
+        title: "Declarative",
         description:
-          "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam ",
+          "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore",
         image: "/images/icon1.png"
       },
       {
-        title: "Com",
+        title: "Components",
         description:
-          "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam ",
+          "Lorem ipsum dolor sit amet, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam ",
         image: "/images/icon2.png"
       },
       {
-        title: "Single",
+        title: "Single-Way",
         description:
-          "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam ",
+          "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor",
         image: "/images/icon3.png"
       },
       {
@@ -39,31 +39,34 @@ class App extends Component {
     return (
       <div className="App">
         <div class="backgroundimage">
-          <div class="logoMenu">
+          <div className="logoMenu">
             <img src="/images/ironhack-logo.svg" class="logo" />
             <img src="/images/menu-top.svg" class="menu" />
           </div>
-          <div class="top">
-            <h1> Say hello to ReactJS</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-              erat, sed diam
+          <div className="top">
+            <h1 style={{ color: "white", fontSize: "60px" }}>
+              {" "}
+              Say hello to <br />
+              ReactJS
+            </h1>
+            <p style={{ color: "white" }}>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, <br />
+              sed diam nonumy eirmod tempor invidunt <br />
+              ut labore et dolore magna aliquyam.
             </p>
-            <button>Awesome</button>
+            <div className="button">Awesome</div>
           </div>
-        </div>
-
-        <div class="flex">
-          {liste.map(e => {
-            return (
-              <div>
-                <h1> {e.title}</h1>
-                {e.description}
-                <img src={e.image} />
-              </div>
-            );
-          })}
+          <div className="feature">
+            {liste.map(e => {
+              return (
+                <div>
+                  <h1> {e.title}</h1>
+                  {e.description}
+                  <img src={e.image} />
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     );
