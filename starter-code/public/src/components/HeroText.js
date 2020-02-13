cd76eee43097048c6a33fdc715f7a1e3dbadbc84
LIZ <liz@MacBook-Air-de-LIZ.local>
2020-02-13 16:53:01 -0600

    in progress

---
+++ starter-code/public/src/components/HeroText.js
@@ -0,0 +1,14 @@
+  
+import React, { Component } from 'react';
+import './HeroText.css'
+
+
+const HeroText = (props) => {
+  return ( 
+    <div className="HeroText">
+      <p>You will learn a frontend framework from scratch, to become a Ninka Developer.</p>
+    </div>
+   );
+}
+ 
+export default HeroText;
---
+++ starter-code/public/src/layout/Hero.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Hero.css'
+
+
+const Hero = (props) => {
+  return ( 
+    <div className="Hero">
+      <h1>Say hello to ReactJS</h1>
+    </div>
+   );
+}
+ 
+export default Hero;
