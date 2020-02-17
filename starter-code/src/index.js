df76cc63f5fce290afa519d88893d9798d385e9e
Paul Dkn <pauldkn.code@gmail.com>
2020-02-17 14:53:45 +0100

    Global config

---
+++ starter-code/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App"; 
+// can be App.js or App.jsx, but webpack read both extensions
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
