16ca140f6687affa1645069fd30f0da31d25d463
Nina Gautreau <nina.gautreau@gmail.com>
2020-02-17 16:50:32 +0100

    hrubfizhkcunkx

--- starter-code/src/components/Main.jsx
+++ starter-code/src/components/Main.jsx
@@ -5,9 +5,11 @@ import "./../styles/App.css";
 const Main = () => {
     return (
         <div className="main-block">
-            <h1>Say Hello to<br/>ReactJS</h1>
-            <p>You will learn a FrontEnd
-                <br/>framwork from Scratch, to
+            <h1>Say Hello to
+                <br/>ReactJS
+            </h1>
+            <p>You will learn a Front-end
+                <br/>framework from scratch, to
                 <br/>become a Ninja Developer.
             </p>
         </div>
