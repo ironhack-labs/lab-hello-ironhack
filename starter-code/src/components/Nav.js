8cc45f65eab292d9bba8585498790fa2850676f5
Ardash <ardache@hotmail.com>
2020-02-18 17:18:15 -0600

    done :+1:

--- starter-code/src/components/Nav.js
+++ starter-code/src/components/Nav.js
@@ -3,9 +3,12 @@ import React, {Component} from 'react';
 class Nav extends Component {
     render() {
         return (
-            <div className = "nav">
-              <img src="../images.ironhack-logo.svg" />
-            </div>
+        
+                <div className = "Navbar">
+                <img src='/images/ironhack-logo.svg'></img>
+                <img src='/images/menu-top.svg'></img>
+                </div>
+           
         )
     }
 }
