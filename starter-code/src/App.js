d8fdfb3274ff780a2969f11f3dd4a3daa530ef36
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-02-24 18:31:45 +0100

    lost too much time trying to fix webpack version issue. Style half way done so i can focus on second lab

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -14,37 +14,42 @@ class App extends Component {
           <img src="/images/menu-top.svg" />
         </nav>
 
-        <h1> Sayzdgsdgs hello to ReactJS </h1>
-        <p>You will learn from scratch to become a Ninja Developer.</p>
-        <button>Awesome!</button>
+        <h1> Say hello to ReactJS </h1>
+        <p>You will learn a Frontend framework from scratch to become a Ninja Developer.</p>
+        <div>
+          <button>Awesome!</button>
+        </div>
+        
       </section>
 
 
       <section class="lower-section">
+        <div class="cards-container">
+          <div className="card">
+              <img src="/images/icon1.png" />
+              <h4>Declarative</h4>
+              <p>React makes it painless to create interactive UIs</p>
+          </div>
 
-        <div className="card">
+          <div className="card">
             <img src="/images/icon1.png" />
             <h4>Declarative</h4>
             <p>React makes it painless to create interactive UIs</p>
-        </div>
+          </div>
 
-        <div className="card">
-          <img src="/images/icon1.png" />
-          <h4>Declarative</h4>
-          <p>React makes it painless to create interactive UIs</p>
-        </div>
+          <div className="card">
+            <img src="/images/icon1.png" />
+            <h4>Declarative</h4>
+            <p>React makes it painless to create interactive UIs</p>
+          </div>
 
-        <div className="card">
-          <img src="/images/icon1.png" />
-          <h4>Declarative</h4>
-          <p>React makes it painless to create interactive UIs</p>
+          <div className="card">
+            <img src="/images/icon1.png" />
+            <h4>Declarative</h4>
+            <p>React makes it painless to create interactive UIs</p>
+          </div>
         </div>
-
-        <div className="card">
-          <img src="/images/icon1.png" />
-          <h4>Declarative</h4>
-          <p>React makes it painless to create interactive UIs</p>
-        </div>
+       
       </section>
 
 
