1fa6438f1d314ce397e5e38fe6b2e39b7e7b6bfc
Michael Mueller <michael@miniguide.co>
2020-04-08 16:22:26 +0200

    layout completed of header and main

--- hello-ironhack/src/App.js
+++ hello-ironhack/src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="/images/ironhack-logo.svg" className="App-logo" alt="logo" />
+        <img src="/images/menu-top.svg" className="App-menu-icon" alt="menu icon" />
       </header>
+      <main className="App-main">
+        <div className="App-main-left">
+          <div>
+            <h1>
+                Say hello to ReactJS
+            </h1>
+            <p>
+              You will learn a Frontend framework from scratch, to become a Ninja Developer.
+            </p>
+          </div>
+          <button className="App-button">
+            Awesome!
+          </button>
+        </div>
+        <div className="App-main-right">
+          <div class="div-1">
+            <img alt="React logo" src="/images/react-logo.svg" />
+          </div>
+          <div class="div-2">
+            <img alt="React logo" src="/images/react-logo.svg" />
+            <img alt="React logo" src="/images/react-logo.svg" />
+            <img alt="React logo" src="/images/react-logo.svg" />
+          </div>
+          <div class="div-3">
+            <img alt="React logo" src="/images/react-logo.svg" />
+            <img alt="React logo" src="/images/react-logo.svg" />
+          </div>
+        </div>
+      </main>
+      <div className="benefits">
+        benef>ts
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ hello-ironhack/src/components/Tile.js
@@ -0,0 +1,15 @@
+// components/Tile.js
+
+import React from 'react';
+
+const tile = () => {
+  return (
+    <div className="benefits-tile">
+      <img src="" className="benefits-icon" alt="" />
+      <h2 className="App-title">fee</h2>
+      <p>foo</p>
+    </div>
+  );
+}
+
+export default tile;
