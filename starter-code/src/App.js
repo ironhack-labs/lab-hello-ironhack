4f32e360421c92ace38d79f378f7961a6b211fe6
Guillaume Marcellin <marcellinguillaume@hotmail.fr>
2020-05-07 09:41:40 +0200

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -14,16 +14,16 @@ class App extends Component {
         <div className="assets">
         <ReactAssets picture="images/icon1.png" 
         title="Declarative" 
-        description="React makes it painless to create interactive UIs"/>
+        description={<p>React makes it <br/> painless to create <br/> interactive UIs</p>}/>
         <ReactAssets picture="images/icon2.png" 
         title="Components" 
-        description="Build encapsulated components that manage their state"/>
+        description={<p>Build encapsulated <br/> components that <br/> manage their state</p>}/>
         <ReactAssets picture="images/icon3.png" 
         title="Single-Way" 
-        description="A set of immutable values are passed to components"/>
+        description={<p>A set of immutable <br/> values are passed to <br/> components</p>}/>
         <ReactAssets picture="images/icon4.png" 
         title="JSX" 
-        description="Statically-typed designed to run on modern browsers"/>
+        description={<p>Statically-typed,<br/> designed to run on<br/> modern browsers</p>}/>
         </div>
         
         
--- starter-code/src/components/ReactAssets.jsx
+++ starter-code/src/components/ReactAssets.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 function ReactAssets(props) {
+    console.log(props.description)
     return (
         <div className="reactAssest">
             <img src={props.picture} alt=""/>
             <h2>{props.title}</h2>
-            <p>{props.description}</p>
+            {props.description}
         </div>
     )
 }
