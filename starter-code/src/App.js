1e3f489117c55a947e014949f31042931d6392e5
Jacopo <jacopo.rodighiero@icloud.com>
2020-05-11 17:29:04 +0200

    done

---
+++ starter-code/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+            <div className='App'>
+                <img src="/images/ironhack-logo.svg" className='ih-logo' alt=""/>
+                <ul className='burger'>
+                    <li className='line1'></li>
+                    <li className='line2'></li>
+                </ul>
+                <img src='/images/react-logo.svg' className='react-logo'></img>
+                <h1>Say hello to ReactJS</h1>
+                <p>You will learn Frotend freamwork from Scratch, to becaome an Ninka Developer.</p>
+                <button>Awesome!</button>
+            </div>
+            <div className="sections">
+                <section>
+                    <img src="/images/icon1.png" alt=""/>
+                    <h3>Declarative</h3>
+                    <p>React makes it painless to create interactive UIs.</p>
+                </section>
+                <section>
+                <img src="/images/icon2.png" alt=""/>
+                    <h3>Components</h3>
+                    <p>React makes it painless to create interactive UIs.</p>
+                </section>
+                <section>
+                <img src="/images/icon3.png" alt=""/>
+                    <h3>Single-Way</h3>
+                    <p>React makes it painless to create interactive UIs.</p>
+                </section>
+                <section>
+                <img src="/images/icon4.png" alt=""/>
+                    <h3>JSX</h3>
+                    <p>React makes it painless to create interactive UIs.</p>
+                </section>
+            </div>
+        </div>
+    )
+  }
+}
+
+export default App;
