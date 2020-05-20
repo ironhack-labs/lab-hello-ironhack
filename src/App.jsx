fd1eab1a0f426ae080238ba517f7fcd77b19f440
PiaG <piagmdf@gmail.com>
2020-05-20 08:56:47 +0200

    basic react setup; included bootstrap and reactstrap; more content to come

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+// example for importing components fro reactstrap
+import { Button } from 'reactstrap';
+
+class App extends Component {
+    render () {
+
+        // content goes here
+        //const ... = ...
+
+        return (
+            <div> 
+            <img src="/images/ironhack-logo.svg" alt="" />
+            <h1>Say hello to ReactJS</h1>
+            </div>
+
+
+        );
+    }
+
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App.jsx";
+
+// npm i bootstrap required
+import 'bootstrap/dist/css/bootstrap.css';
+import "./App.css";
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
