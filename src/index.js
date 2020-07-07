4c6fd711797a917d08cc1cb46788c22f73ab9b29
Marc De Mena <mdemena@gmail.com>
2020-07-07 20:06:56 +0200

    First Iteration finished

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./Home.js";
+import Services from "./Service.js";
+ReactDOM.render(
+  <Home />,
+  document.getElementById("root")
+);
