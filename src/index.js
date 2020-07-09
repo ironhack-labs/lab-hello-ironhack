e9b928bb8c6768417157c3ec16761ad8a65beaeb
Alex <alex@Alexs-MacBook-Pro.local>
2020-07-09 18:35:50 +0200

    refactored cards. Needs check

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom'
 import "./app.css"
 import Card from './components/Card'
 
+const cards = [
+    {
+        cardTitle: "Declarative", 
+        cardBody:" React makes it painless to create inter UIs",
+        cardImg: "/images/icon1.png"
+    },
+    {
+        cardTitle:"Components", 
+        cardBody:"React makes it painless to create inter UIs", 
+        cardImg:"/images/icon2.png"
+    },
+    {
+        cardTitle:"Single way", 
+        cardBody:"React makes it painless to create inter UIs",
+        cardImg:"/images/icon3.png"
+    },
+    {
+        cardTitle:"JSX", 
+        cardBody:"React makes it painless to create inter UIs",
+        cardImg:"/images/icon4.png"
+    },
+   ]
+
 const myElement = <div>
     <div className="banner"> 
         <div className="navbar">
@@ -15,28 +38,14 @@ const myElement = <div>
             <button>Awesome!</button>
         </div>
     </div>
-    <div className="card-container">
-    
-        <Card cardTitle="Declarative" 
-        cardBody="React makes it painless to create inter UIs" 
-        cardImg="/images/icon1.png"
-        />
-     
-        <Card cardTitle="Components" 
-        cardBody="React makes it painless to create inter UIs" 
-        cardImg="/images/icon2.png"
-        />
-
-        <Card cardTitle="Single way" 
-        cardBody="React makes it painless to create inter UIs"
-        cardImg="/images/icon3.png"
-        /> 
-
- 
-        <Card cardTitle="JSX" 
-        cardBody="React makes it painless to create inter UIs" 
-        cardImg="/images/icon4.png"
-        />    
+    <div className="card-container">   
+            { cards.map(card => 
+            <Card 
+                cardTitle={card.cardTitle} 
+                cardBody= {card.cardBody}
+                cardImg={card.cardImg}
+            />)
+            }
     </div>
 </div>
 
