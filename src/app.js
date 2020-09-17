d7b3e4f919ed3777f014528afba122f0a91a8b87
Matthew Howard <mattalanhoward@gmail.com>
2020-09-18 00:07:23 +0200

    add react logos

--- src/app.js
+++ src/app.js
@@ -14,6 +14,11 @@ const App = () => {
         <h1>Say hello to ReactJS</h1>
         <h3>You will learn a Frontend framework from scratch, to become a Ninja Developer.</h3>
         <button><a href="#">Awesome!</a></button>
+        <img className="logo1" src="../images/react-logo.svg"/>
+        <img className="logo2" src="../images/react-logo.svg"/>
+        <img className="logo3" src="../images/react-logo.svg"/>
+        <img className="logo4" src="../images/react-logo.svg"/>
+
 
     </main>
 
