492be10cf79d76ad3b3d337ce9f3757058422c8c
Raquel Nascimento <raquel.fm.nascimento@gmail.com>
2020-09-17 23:46:09 +0100

    more styling

--- src/app.js
+++ src/app.js
@@ -9,15 +9,24 @@ const App = () => {
           <img src="../images/ironhack-logo.svg"></img>
           <img src="../images/menu-top.svg"></img>
         </nav>
-        <section>
-          <div>
-            <h1>Say hello to ReactJS</h1>
-            <p>
-              You will learn a frontend framework form scratch, to become an
-              amazing developer.
-            </p>
+        <section className="first-div-child">
+          <div className="main-text">
+            <div>
+              <h1>Say hello to ReactJS</h1>
+              <p>
+                You will learn a frontend framework form scratch, to become an
+                amazing developer.
+              </p>
+            </div>
+            <button>Awesome!</button>
           </div>
-          <button>Awesome!</button>
+          <div className="backgrounds">
+            <div className="bg-1"></div>
+            <div className="bg-2"></div>
+            <div className="bg-3"></div>
+
+            <div className="bg-4"></div>
+          </div>
         </section>
       </section>
       <section className="second-div">
