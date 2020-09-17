17a108ebe295c9c28d97d6f63487d428c41acb2d
Matthew Howard <mattalanhoward@gmail.com>
2020-09-18 00:12:04 +0200

    done.  for real.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import './App.css';
-import './index.css';
+// import './index.css';
 
 const App = () => {
   return(
@@ -22,28 +22,7 @@ const App = () => {
 
     </main>
 
-    <section id="cards">
-        <div className="card">
-        <img src="../images/icon1.png" alt="Declarative"/>
-        <h3>Declarative</h3>
-        <p>React makes it painless to create declarative UIs.</p>
-        </div>
-        <div className="card">
-        <img src="../images/icon2.png" alt="Declarative"/>
-        <h3>Components</h3>
-        <p>Build encapsulated componenets that manage their state.</p>
-        </div>
-        <div className="card">
-        <img src="../images/icon3.png" alt="Declarative"/>
-        <h3>Single-Way</h3>
-        <p>A set of immutable values are passed to the componenets.</p>
-        </div>
-        <div className="card">
-        <img src="../images/icon4.png" alt="Declarative"/>
-        <h3>JSX</h3>
-        <p>Statically-typed, designed to run on modern browsers.</p>
-        </div>
-    </section>
+
       
       
 
