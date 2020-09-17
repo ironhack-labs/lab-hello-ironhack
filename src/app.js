09b011cb9c6d6f724f2ccc5f5e120633b7a0b2ea
Cristina Matamoro <cristina.matamoro@gmail.com>
2020-09-17 22:43:06 +0200

    iteration 1,2,3 miss styling

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return(
+    <div>
+      <h1 className='app'>Hi y'all</h1>
+      <h2 className='header'> How are you?</h2>
+    </div>
+  )
+  
+};
+
+export default App;
