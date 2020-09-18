48c4c7458f19924f44c37d4daf16bb71c74dade8
Matthew Howard <mattalanhoward@gmail.com>
2020-09-18 10:28:31 +0200

    move section back into component

--- src/app.js
+++ src/app.js
@@ -22,9 +22,28 @@ const App = () => {
 
     </main>
 
-
-      
-      
+    <section id="cards">
+        <div class="card">
+        <img src="../images/icon1.png" alt="Declarative"/>
+        <h3>Declarative</h3>
+        <p>React makes it painless to create declarative UIs.</p>
+        </div>
+        <div class="card">
+        <img src="../images/icon2.png" alt="Declarative"/>
+        <h3>Components</h3>
+        <p>Build encapsulated componenets that manage their state.</p>
+        </div>
+        <div class="card">
+        <img src="../images/icon3.png" alt="Declarative"/>
+        <h3>Single-Way</h3>
+        <p>A set of immutable values are passed to the componenets.</p>
+        </div>
+        <div class="card">
+        <img src="../images/icon4.png" alt="Declarative"/>
+        <h3>JSX</h3>
+        <p>Statically-typed, designed to run on modern browsers.</p>
+        </div>
+    </section>
 
     </div>
   )
