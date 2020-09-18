42b53f6948a693e6f897141673bdcf9c0a42d01b
Cristina Matamoro <cristina.matamoro@gmail.com>
2020-09-18 22:56:58 +0200

    styling done

--- src/app.js
+++ src/app.js
@@ -8,34 +8,32 @@ const App = () => {
           <img src="../images/ironhack-logo.svg"></img>
           <img src="../images/menu-top.svg"></img>
       </nav>
-      <main>
+      <main className="main-text" >
         <h1 className='app'>Say Hello to ReactJs</h1>
         <h2 className='header'> You will learn a FrontEnd framework from scratch, to become a Ninja Developer</h2>
-        <button>Awesome</button>
+        <button>Awesome!</button>
       </main>
       <section className="container">
-        <div className="row">
             <div className="box">
               <img src="../images/icon1.png" alt="Declarative"/>
-              <h4>Declarative</h4>
+              <h4 className= "box-heading">Declarative</h4>
               <p>React makes it painless to create interactive UIS</p>
             </div>
             <div className="box">
               <img src="../images/icon2.png" alt="Declarative"/> 
-              <h4>Components</h4>
+              <h4 className= "box-heading">Components</h4>
               <p>Build encapsulated components that manage their state</p>
             </div>
             <div className="box">
               <img src="../images/icon3.png" alt="Declarative"/>
-              <h4>Single Way</h4>
+              <h4 className= "box-heading">Single-Way</h4>
               <p>A set of inmutable values are passed to the components</p>
             </div>
             <div className="box">
               <img src="../images/icon4.png" alt="Declarative"/>
-              <h4>JSX</h4>
+              <h4 className= "box-heading">JSX</h4>
               <p>Statically typed,designed to run on modern browsers</p>
             </div>
-        </div>
       </section>
  </div>
   )
