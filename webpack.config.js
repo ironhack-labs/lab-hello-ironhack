f035c4c2f5d0cef98240751e3a465f2c4c93ad7e
Andrew <andre.mastercode@gmail.com>
2020-09-19 07:41:50 +0000

    done

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const bundlePath = path.resolve(__dirname, "dist/");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
