f40dceb80f8d15921635615efaf1bc978225e940
Hamlet <hayrapetyan.hamlet@gmail.com>
2020-09-21 15:05:33 +0200

    Iteration 3 on the way to perfection

--- src/index.js
+++ src/index.js
@@ -2,11 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
+const navBar = (
+    <nav>
+    <img class="logo" src="../images/ironhack-logo.svg"/>
+    <img src="../images/menu-top.svg"/>
+    </nav>
+)
+
 const greeting = (
-    <h1 style={{color: 'white'}}>Say Hello to ReactJS</h1>
+<div> 
+    <h1>Say Hello to ReactJS</h1>
+    <p>You will learn a FrontEnd framework from scratch, to become a Ninka Developer.</p>
+    <button>Awesome!</button>
+</div>
+)
+
+const reactLogo = (
+    <img src="../images/react-logo.svg"/>
+)
+
+const body = (
+    <div>
+        {navBar}
+        {greeting}
+        {reactLogo}
+    </div>
 )
 
 ReactDOM.render(
-    greeting,
+    body,
     document.getElementById('root')
 )
