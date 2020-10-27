e9ea8706cb15bd822962d92bcd96fdb12de8d5fc
Tami Pipoca <tamisakita@hotmail.com>
2020-10-26 21:49:45 -0300

    finished lab

--- src/app.js
+++ src/app.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
-import { ReactComponent as logoIronhack } from '../public/images/ironhack-logo.svg';
-
-const logo = () => {
-  return (
-    <div>
-      <logoIronhack />
-    </div>
-  )
-};
+import "./app.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>Say hello to ReactJS</h1>
-      <p>You will learn how to use the most popular frontend library, and become a super Ninja developer.</p>
-      <Button variant="secondary">Awesome!</Button>{' '}
-    </div>
+    <main>
+      <section className="main-section-container">
+        <div className="main-section-images">
+          <img src="/images/ironhack-logo.svg" alt="logo" />
+          <img src="/images/menu-top.svg" alt="menu" />
+        </div>
+
+        <div className="main-section-text">
+          <h1>Say hello to ReactJS</h1>
+          <p>You will learn how to use the most popular frontend library, and become a super Ninja developer.</p>
+          <button>Awesome!</button>
+        </div>
+      </section>
+      
+
+      <section className="items-section">
+        <div>
+          <img className="images" src="/images/icon1.png" />
+          <h2>Declarative</h2>
+          <p className="text-information">React makes it painless to create interactive UIs</p>
+        </div>
+        <div>
+          <img className="images" src="/images/icon2.png" />
+          <h2>Components</h2>
+          <p className="text-information">Build encapsulated components that manage their state</p>
+        </div>
+        <div>
+          <img className="images" src="/images/icon3.png" />
+          <h2>Single-Way</h2>
+          <p className="text-information">A set of immutable values are passed to the component's</p>
+        </div>
+        <div>
+          <img className="images" src="/images/icon4.png" />
+          <h2>JSX</h2>
+          <p className="text-information">Statically-typed designed to run on modern browsers</p>
+        </div>
+      </section>
+    </main>
   )
 };
 
 export default App;
-export default logo;
