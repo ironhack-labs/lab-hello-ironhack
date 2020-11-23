7d48a327b1ebc44b54d32eb0f8a347088089e846
Juan Manuel <jramirezm942@gmail.com>
2020-11-22 19:26:30 -0600

    Por ahora ya tengo la conexion del servidor con el react en el locl host 300

---
+++ src/App.js
@@ -0,0 +1,35 @@
+// 1.- importaciones
+import React, { Component } from "react";
+
+export default class App extends Component{
+    render(){
+        return(
+            <h1> LABORATORIO IRON HACK REACT </h1>
+            )
+    }
+}
+
+
+
+
+
+// import "./App.css";
+// // 2.- funcion o clase
+// class App extends Component {
+//     render() {
+
+        
+
+        
+//       return (
+//         <div className="App">
+//           <h1> Hello Ironhackers!  </h1>
+//           <h5>ESTE ES EL PRIMER EJEMPLO CON REACT WEB :)</h5>
+//         </div>
+//       );
+//     }
+//   }
+
+   
+//   export default App;
+// // 3.- exportaciones
---
+++ src/index.js
@@ -0,0 +1,12 @@
+//1.- importciones
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+
+
+//2.- funciones o clases
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+  );
+//3.- exportaciones
