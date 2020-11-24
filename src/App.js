9a19bfa72368d9bf889621180a18057b1b6a7e91
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-23 18:58:26 -0600

    oon 3000

--- src/App.js
+++ src/App.js
@@ -6,12 +6,28 @@ import image from './foto.png'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src={image} style={{width: '500px'
-        }}/>
-        
-      </header>
+
+      <section id="blueRect">
+        <header class="header">
+          <div id="letrasBlancas">
+            <h1>Say hello to reactJS</h1>
+            <br/>
+
+          </div>
+          <div class="parrafosBlancos">
+            <p>You will learn how to use </p>
+            <p>the most popular front end library</p>
+            <p>and become a super ninja developer</p>
+
+          </div>
+          <button type="button" class="btn btn-light">Awesome!</button>
+
+        </header>
+      </section>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+
     </div>
   );
 }
