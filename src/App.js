690c196ac11a1746393a921c44ea43a1ffdc5a93
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-23 19:16:37 -0600

    piics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import image from './foto.png';
+import icon1 from './images/icon1.png';
+import icon2 from './images/icon2.png';
+import icon3 from './images/icon3.png';
+import icon4 from './images/icon4.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -8,7 +12,6 @@ function App() {
     <div className="App">
 
       <section id="blueRect">
-        <header class="header">
           <div id="letrasBlancas">
             <h1>Say hello to reactJS</h1>
             <br/>
@@ -22,10 +25,31 @@ function App() {
           </div>
           <button type="button" class="btn btn-light">Awesome!</button>
 
-        </header>
       </section>
-      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+
+     <section id="threeElements">
+       <h2 class="h2-underline">What’s on Spotify?</h2>
+       <div class="h2-wrapper">
+         <div class="container">
+           <img src="./images/music-icon.png" alt="Millions of Songs on Spotify"/>
+           <h3> Millions off Songs</h3>
+           <p> There are millions of songs on Spotify</p>
+         </div>
+         <div class="container">
+           <img src="./images/high-quality-icon.png" alt="Enjoy high quality music on Spotify"/>
+           <h3> HD Music</h3>
+           <p>Listen to music as if you were listening live</p>
+         </div>
+         <div class="container">
+           <img src="./images/devices-icon.png" alt="Stream everywhere with Spotify"/>
+           <h3>Stream Everywhere</h3>
+           <p> Stream music on your smartphone, tablet or computer</p>
+         </div>
+       </div>
+     </section>
+
+
 
 
     </div>
