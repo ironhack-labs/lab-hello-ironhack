1207f69eaa47991b9e6e766a31b75bfe84692959
Leonardo Campuzano Magallanes <leonardocampuzano43@gmail.com>
2020-11-24 00:55:33 -0600

    Styling pending

---
+++ lab-hello-ironhack/src/Hello.js
@@ -0,0 +1,35 @@
+const Hello = () => {
+    return <div>
+        <div>
+            <img src="./public/images/ironhack-logo.svg"></img>
+            <img src="./public/images/menu-top.svg"></img>
+            <h1>Say Hello to React JS</h1>
+            <p>You will learn how to use the most popular frontend library and become a super ninja developer.</p>
+            <button>Awesome</button>
+        </div>
+        <div className="div-section2">
+            <div>
+                <img src="./public/images/icon1.png"></img>
+                <h2>Declarative</h2>
+                <p>Lorem</p>
+            </div>
+            <div>
+                <img src="./public/images/icon2.png"></img>
+                <h2>Components</h2>
+                <p>Lorem</p>
+            </div>
+            <div>
+                <img src="./public/images/icon3.png"></img>
+                <h2>Single Way</h2>
+                <p>Lorem</p>
+            </div>
+            <div>
+                <img src="./public/images/icon4.png"></img>
+                <h2>JSX</h2>
+                <p>Lorem</p>
+            </div>
+        </div>
+    </div>
+}
+
+export default Hello
