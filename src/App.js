17dfc432ef75e91b93bea89df9dda466abc56c60
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-23 19:22:36 -0600

    color

--- src/App.js
+++ src/App.js
@@ -29,21 +29,26 @@ function App() {
 
 
      <section id="threeElements">
-       <h2 class="h2-underline">What’s on Spotify?</h2>
        <div class="h2-wrapper">
          <div class="container">
-           <img src="./images/music-icon.png" alt="Millions of Songs on Spotify"/>
-           <h3> Millions off Songs</h3>
+           <img src={icon1} alt="Millions of Songs on Spotify"/>
+           <h3> Declarative</h3>
            <p> There are millions of songs on Spotify</p>
          </div>
          <div class="container">
-           <img src="./images/high-quality-icon.png" alt="Enjoy high quality music on Spotify"/>
-           <h3> HD Music</h3>
+         <img src={icon2} alt="Millions of Songs on Spotify"/>
+
+           <h3> Components</h3>
            <p>Listen to music as if you were listening live</p>
          </div>
          <div class="container">
-           <img src="./images/devices-icon.png" alt="Stream everywhere with Spotify"/>
-           <h3>Stream Everywhere</h3>
+         <img src={icon3} alt="Millions of Songs on Spotify"/>
+           <h3>Single Ways</h3>
+           <p> Stream music on your smartphone, tablet or computer</p>
+         </div>
+         <div class="container">
+         <img src={icon4} alt="Millions of Songs on Spotify"/>
+           <h3>JSX</h3>
            <p> Stream music on your smartphone, tablet or computer</p>
          </div>
        </div>
