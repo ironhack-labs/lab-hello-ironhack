8a3986dfd2cdc5ca3b3c9ccf46f18958137347c4
Adrián <adrianmonzonf@gmail.com>
2020-11-30 17:03:33 +0100

    intentado

---
+++ hello-ironhack-app/src/components/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Title from './Title'
+
+function App() {
+  return (
+    <main>
+
+    <Title text="Say hello to ReactJS" />
+
+    </main>
+  );
+}
+
+export default App;
---
+++ hello-ironhack-app/src/components/Title.js
@@ -0,0 +1,13 @@
+
+const Title = props => {
+    
+return (
+
+    <>
+    <h3 className="titulo">{props.text}</h3> 
+    <p>hola</p>
+    </>
+)
+}
+
+export default Title
