e08138f640aae19f602566cf7a894185c899abe5
Alicja Slupska <ala.slupska@gmail.com>
2020-12-12 12:50:25 +0100

    basic app setup to make it run

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+ 
+class App extends Component {
+  render() {
+    return (
+        // like the hbs template in backend
+      <div className="App">
+        <h1> Hello Ironhackers! </h1>
+        
+      </div>
+    );
+  }
+}
+ 
+export default App;
