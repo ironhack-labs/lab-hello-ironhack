1e53150c55666fc525094d541f5c65f50080e4f1
Zhenni Qian <zhenni.qian913@gmail.com>
2021-01-15 23:13:20 +0100

    done, styled just a little bit

--- src/app.js
+++ src/app.js
@@ -3,11 +3,36 @@ import './app.css';
 
 const App = () => {
     return <div>
-        <div className="manifesto">
+        <header>
             <nav>
                 <img src="./images/ironhack-logo.svg"/>
-                <p>test</p>
+                <img src="./images/menu-top.svg" />
             </nav>
+            <h1>Say hello to ReactJS</h1>
+            <p>You will learn how to use the most popular frontend library, and become a super Ninja developer.</p>
+            <button>Awesome!</button>
+        </header>
+        <div className="characteristics">
+            <div>
+                <img src="./images/icon1.png" />
+                <h2>Declarative</h2>
+                <p>React makes it painless to create interactive UIs.</p>
+            </div>
+            <div>
+                <img src="./images/icon2.png" />
+                <h2>Components</h2>
+                <p>Build encapsulated components that manage their state.</p>
+            </div>
+            <div>
+                <img src="./images/icon3.png" />
+                <h2>Single-Way</h2>
+                <p>A set of immutable values are passed to the component's.</p>
+            </div>
+            <div>
+                <img src="./images/icon4.png" />
+                <h2>JSX</h2>
+                <p>Statically-typed. designed to run on modern browsers.</p>
+            </div>
         </div>
     </div>;
 };
