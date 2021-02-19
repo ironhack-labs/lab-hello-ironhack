315216148db121638c0af61d1ba8ca9938d81f57
RFeldman84 <rachel.feldman84@gmail.com>
2021-02-19 11:07:36 -0500

    can't get to set up to try but here you go

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+//****THIS WONT SET UP AND WORK RIGHT SO CAN'T FINISH LAB I GIVE UP NEED HELP IT'S MY COMPUTER OR ME? LOL 
+//****** JUST KNOW I TRIED... A LOT! BAHHH 
+const App = () => {
+ 
+  return (
+  <header>
+    <div>
+      <h1>Hello World!</h1>;
+    </div>
+  </header>
+    
+  )
+
+};
+
+
+
+ 
+
+
+
+
+export default App;
