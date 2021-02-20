c16e833071de8b019e237bd2c36b6d9988c0b59f
Sebastian Montoya <seb246@gmail.com>
2021-02-20 01:21:22 -0500

    started component 1 still need to style

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Say hello ReactJS</h1>
+            <p>You will learn how to use the most popular frontend library, and become a super ninja developer</p>
+            <button>Awesome!</button>
+        </div>
+    )
+}
+
+export default Header
