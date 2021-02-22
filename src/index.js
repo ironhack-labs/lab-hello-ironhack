61ebd268a818c11507e1438ef9dcd5c7b8345d95
Mafe Bracho <mafe.bracho@gmail.com>
2021-02-22 12:39:55 +0100

    start styling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// const element = (
+//     <h1>
+//       Hello, {use}!
+//     </h1>
+//   );
+
+  const myStyle = {
+    height: "100vh",
+    backgroundColor: "midnightblue",
+    // margin: 0,
+    // padding: 0
+  };
+  
+  const element = (
+      <div style={myStyle}>
+      <h1>
+          Say hello to
+      </h1>
+      <h1>
+          ReactJS
+      </h1>
+      <p>
+          You will learn how to use
+      </p>
+      <p>
+          the most popular frontend library,
+      </p>
+      <p>
+          and become a super Ninja developer.
+      </p>
+
+      <button type="button">Awesome!</button>
+
+      </div>
+  );
+
 ReactDOM.render(
-    'Hello World',
+    element,
     document.getElementById('root')
 );
