9e95cf24c94f420e5af9c44f8bf226ca48de206a
Thiarhes <injesusservice@hotmail.com>
2021-03-12 12:25:04 -0300

    npm run webpack does not work

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import IronLogo from './ironhackLogo/IronhackLogo.js';
+
+const App = () => {
+    return (
+        <div>
+            <IronLogo />
+        </div>
+    )
+}
+
+
+export default App;
---
+++ src/ironhackLogo/IronhackLogo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Logo from '../public/images/ironhack-logo.svg'
+
+function IronLogo() {
+    return <div>{Logo}</div>
+}
+
+
+export default IronLogo;
