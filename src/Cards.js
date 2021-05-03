db4edd760cf07fa7f2a03a56f89d64f674f004c1
Pablo del Barrio <pablodbb@gmail.com>
2021-05-03 16:11:32 +0200

    Lab done!:white_check_mark:

--- src/Cards.js
+++ src/Cards.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import './Cards.css'
 
-// const Cards = () => {
-//   return (
-//     <div className="cards">
-//     <h2></h2>
-//     <img src="/images/icon1.png"/>
-//     </div>
-//   )
-// }
 
-function Cards ({ cardImage, cardTitle, cardText }) {
+
+const Cards = ({ cardImage, cardTitle, cardText}) =>{
     return (
       <div className="cards">
       <img className="img-card" src={cardImage}/>
@@ -18,6 +11,5 @@ function Cards ({ cardImage, cardTitle, cardText }) {
       <p>{cardText}</p>
       </div>
     )
-  }
-
+}
 export default Cards;
