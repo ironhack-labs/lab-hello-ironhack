106769b287515379fb41b7d9f9a7cb721b2522a0
Tigran Harutyunyan <tigrandeboris@outlook.com>
2021-05-03 15:27:38 +0200

    ADD initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HeaderComponent from './HeaderComponent';
+import './App.css';
+
+
+function App() {
+    return (
+        <div className='App'>
+            <HeaderComponent />
+        </div>
+    )
+
+}
+
+export default App;
---
+++ src/HeaderComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Header () {
+    return (
+        <div>
+            <h1>Say Hello to ReactJS</h1>
+            <p>Texto</p>
+            <button>Awesome!</button>
+        </div>
+    )
+}
+
+export default Header;
