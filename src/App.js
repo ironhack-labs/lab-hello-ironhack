f828266ab703be38e31a53ff41258ac94ad9a050
andres_rog <andres_rog@outlook.com>
2021-06-14 22:44:19 -0500

    lab completado

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, {
+    Component
+} from 'react';
+import "./App.css";
+
+import ContentCard from "./components/Card";
+
+export default class App extends Component {
+
+    render(){
+        const cards = [{
+            pic: "/images/icon1.png",
+            title: "Declarative",
+            description: "React makes it painless to create interactive UIs."
+        },
+        {
+            pic: "/images/icon2.png",
+            title: "Components",
+            description: "Build encapsulated components that manage their state."
+        },
+        {
+            pic: "/images/icon3.png",
+            title: "Single-Way",
+            description: "A set of immutable values are passed to the component's."
+        },
+        {
+            pic: "/images/icon4.png",
+            title: "JSX",
+            description: "Statically-typed, designed to run on modern browsers."
+        }
+    ];
+        return(
+            <div className="App">
+                <section className="head">
+                    <div className="top">
+                        <img className="logo" src="/images/ironhack-logo.svg"/>
+                        <img className="topMenu" src="/images/menu-top.svg"/>
+                    </div>
+                    <div className="title">
+                        <h1>Say hello to ReactJS</h1>
+                        <h3>You will learn how to use the most popular frontend library, and become a super Ninja developer.</h3>
+                        <button><h3>Awesome!</h3></button>
+                    </div>
+                </section>
+                <section className="body">
+                    {cards.map((card,index_card)=>(
+                        <ContentCard
+                        key={index_card}
+                        pic={card.pic}
+                        title={card.title}
+                        description={card.description}
+                        />
+                    ))}
+                </section>
+
+            </div>
+        )
+    }
+}
