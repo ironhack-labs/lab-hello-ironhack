cd42c35d6f1809998a6b84bc6d9874f91903f9b4
Ana Carolina R Lima <anacarolina.rdel@gmail.com>
2021-07-12 21:46:19 -0300

    done

--- src/App.js
+++ src/App.js
@@ -5,34 +5,49 @@ const App = () => {
     return (
     <div className='App'>
         <div className='Container'>
-            <div>
-                <img src="../images/ironhack-logo.svg" alt="ironhack-img" />
-                <img src="../images/menu-top.svg" alt="menu-img" />
+            <div id='nav-bar'>
+                <img src="../images/ironhack-logo.svg" alt="ironhack-img" id='ironhack-img'/>
+                <img src="../images/menu-top.svg" alt="menu-img" id='menu-img' />
             </div>
-            <h1>Say Hello to ReactJS</h1>
-            <p>You will learn how to use the most popular frontend library, and become the Super Ninja developer.</p>
-            <button>Awesome!</button>
+            <div id="title-div">
+                <div id="text-div">
+                    <h1 id='title'>Say hello to <br/>
+                    ReactJS</h1>
+                    <p id='paragraph'>You will learn how to use <br/>
+                        the most popular frontend library, <br/>
+                        and become the Super Ninja developer.</p>
+                    <button id='button'>Awesome!</button>
+                </div>
+            </div>
         </div>
         <div className='Images'>
             <div>
                 <img src="../images/icon1.png" alt="icon1-img" />
                 <h3>Declarative</h3>
-                <p>React makes it painless to creat interactive UIs.</p>
+                <p>React makes it <br/>
+                painless to creat <br/>
+                interactive UIs.</p>
             </div>
             <div>
                 <img src="../images/icon2.png" alt="icon2-img" />
                 <h3>Components</h3>
-                <p>Build encapsulated components that manage their state.</p>
+                <p>Build encapsulated <br/>
+                components that <br/>
+                manage their state.</p>
             </div>
             <div>
                 <img src="../images/icon3.png" alt="icon3-img" />
                 <h3>Single-way</h3>
-                <p>A set of immutable values are passed to component's.</p>
+                <p>A set of immutable <br/>
+                values are passed to <br/>
+                the component's.</p>
             </div>
             <div>
                 <img src="../images/icon4.png" alt="icon4-img" />
                 <h3>JSX</h3>
-                <p>Statically-typed, designed to run on modern browsers.</p>
+                <p>Statically-typed, <br/>
+                designed to run on <br/>
+                modern browsers.</p>
             </div>
         </div>
     </div>
