2e4e26033ce1fbf297bbca480037febc8dbea876
P4tt1XD <patti@outlook.com.br>
2021-07-13 21:24:29 -0300

    Done

---
+++ app.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css";
+
+const App = () =>{
+    return (
+        <div className="total">
+            <div>
+                <h1 id="title"> Say Hello to <br/>React </h1>
+                <p> You will learn how to use<br/> the most popular frontend library,
+                <br/> and become a super ninja developer. </p>
+                <h3> Declarative </h3>
+                <p> React make it <br/>
+                painless to create <br/>
+                interactive UIs. </p>
+                <h3> Components </h3>
+                <p> Build encapsulated <br/>
+                components that <br/>
+                manage their state. </p>
+                <h3> Single way </h3>
+                <p> A set of immutable <br/>
+                values are passed  to <br/>
+                the component's. </p>
+                <h3> JSX </h3>
+                <p> Statically-typed <br/>
+                designed to run on <br/>
+                modern browser </p>
+                </div>
+     </div>
+    );
+}
