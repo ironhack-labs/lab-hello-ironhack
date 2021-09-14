a4e7041557ed5474edf1b961bbd8cc51cea5df56
juliahaeussler <julia@haeuszler.de>
2021-09-14 14:35:59 +0200

    lab done

--- src/App.js
+++ src/App.js
@@ -11,20 +11,17 @@ function App () {
             
             <section className="top">
                 <div className="navbar">
-                        <img src="/images/ironhack-logo.svg" alt="ironhack logo"/>
-                        <img src="/images/menu-top.svg" alt="menu"/>
+                    <img src="/images/ironhack-logo.svg" alt="ironhack logo"/>
+                    <img src="/images/menu-top.svg" alt="menu"/>
                 </div>
-
                 <div className="top-text">
                     <h1>Say hello to ReactJS</h1>
                     <p className="headline-text">You will learn how to use the most popular frontend library, and become a super Ninja developer.</p>
                     <button onClick={Awesome}>Awesome!</button>  
                 </div>
-
             </section>
 
            <section className="bottom">
-                
                     <div>
                         <img src="/images/icon1.png" alt="icon1" />
                         <h3>Declarative</h3>
@@ -48,8 +45,6 @@ function App () {
                         <h3>JSX</h3>
                         <div>Statically-typed, designed to run on modern browsers.</div>
                     </div>
-                
-
            </section>
 
         </main>
