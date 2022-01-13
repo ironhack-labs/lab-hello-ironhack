06d083cdb914a40a33e051d43ae19398da8f1276
Brigi Horvath <brigi.horvath82@gmail.com>
2022-01-13 21:12:36 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar">
+        <img src="/images/ironhack-logo.svg" alt="logo" />
+        <img src="/images/menu-top.svg" alt="logo" />
+      </nav>
+      <h1>Say hello to ReactJS</h1>
+      <img src="/images/react-logo.svg" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
