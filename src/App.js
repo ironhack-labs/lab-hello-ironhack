7a3ec90642cd0b904c59453480b3c8810c8d0f3e
phimanim <aliker.imane@gmail.com>
2022-01-14 11:24:09 +0100

    lab solved

---
+++ src/App.js
@@ -0,0 +1,38 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+      <div className="App-navbar">
+      <img
+          src="/images/ironhack-logo.svg"
+          alt="ironhack logo"
+          className="ironhack-logo"
+        />
+      <img
+          src="/images/menu-top.svg"
+          alt="menu top img"
+          className="menu-top"
+        />
+      </div>
+<div className="App-text">
+        <h1>
+          Say hello to ReactJS
+        </h1>
+        <p>
+        You will learn how to use the most popular frontend library, and become a super Ninja Developer.
+        </p>
+       
+
+        <button>Awesome !</button>
+</div>
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
